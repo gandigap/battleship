094fceef1a6b56fa45908f97012ece3170b10ebd
Ihar Berasneu <iberesnev@alfabank.ru>
2023-07-07 22:20:36 +0300

    feat: add config from crud api for current task

---
+++ src/http_server/index.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import { createServer } from 'http';
+import path from 'path';
+
+export const httpServer = createServer((req, res) => {
+  const dirname = path.resolve(path.dirname(''));
+  const filePath = dirname + (req.url === '/' ? '/front/index.html' : `/front${req.url}`);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
+
+export default httpServer;
